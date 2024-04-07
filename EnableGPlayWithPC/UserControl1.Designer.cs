5573d4e02f03f8d1ff975f5f816f7df3fa894983
Kobold831 <146227823+Kobold831@users.noreply.github.com>
2024-04-07 18:36:23 +0900

    修正

--- EnableGPlayWithPC/UserControl1.Designer.cs
+++ EnableGPlayWithPC/UserControl1.Designer.cs
@@ -43,18 +43,18 @@
             this.checkedListBox1.Margin = new System.Windows.Forms.Padding(0);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.ScrollAlwaysVisible = true;
-            this.checkedListBox1.Size = new System.Drawing.Size(1124, 312);
+            this.checkedListBox1.Size = new System.Drawing.Size(899, 234);
             this.checkedListBox1.TabIndex = 23;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(0, 343);
+            this.textBox1.Location = new System.Drawing.Point(0, 274);
             this.textBox1.Margin = new System.Windows.Forms.Padding(0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(892, 179);
+            this.textBox1.Size = new System.Drawing.Size(714, 144);
             this.textBox1.TabIndex = 24;
             // 
             // button
@@ -64,10 +64,10 @@
             this.button.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button.Location = new System.Drawing.Point(898, 442);
-            this.button.Margin = new System.Windows.Forms.Padding(6, 12, 6, 12);
+            this.button.Location = new System.Drawing.Point(718, 354);
+            this.button.Margin = new System.Windows.Forms.Padding(5, 10, 5, 10);
             this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(220, 80);
+            this.button.Size = new System.Drawing.Size(176, 64);
             this.button.TabIndex = 25;
             this.button.Text = "実行";
             this.button.UseVisualStyleBackColor = false;
@@ -77,10 +77,9 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.linkLabel1.Location = new System.Drawing.Point(4, 530);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(3, 424);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(109, 25);
+            this.linkLabel1.Size = new System.Drawing.Size(87, 20);
             this.linkLabel1.TabIndex = 26;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "linkLabel1";
@@ -88,8 +87,8 @@
             // 
             // UserControl1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoSize = true;
             this.Controls.Add(this.linkLabel1);
@@ -99,7 +98,7 @@
             this.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "UserControl1";
-            this.Size = new System.Drawing.Size(1124, 555);
+            this.Size = new System.Drawing.Size(899, 444);
             this.ResumeLayout(false);
             this.PerformLayout();
 
